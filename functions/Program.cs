4441f7745831a9e8059913d1af36ef00664da37d
Opilane TTHK <opilane@tthk.ee>
2019-10-17 17:05:25 +0300

    first commit

---
+++ functions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //DRY
+            //do not
+            //repeat
+            //yourself
+
+           /* int a = 5;
+            int b = 5;
+            int c = a + b;
+
+            Console.WriteLine($"Result from main {c}");
+            Sum(a, b);
+            Sum(10, 20);
+            //Sum("five", "six");*/
+            Hello();
+            string name;
+
+            GreetByName {name};
+        }
+        
+
+            
+
+        public static void Sum(int x, int y)
+        {
+            Console.WriteLine($"Result: {x + y}.");
+        }
+
+        public static void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public static void GreetByName()
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+
+    }
+}
---
+++ myfunction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace myfunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string color;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("What is your favourite color?");
+            color = Console.ReadLine();
+            KnowMe(name, color);
+        }
+            
+
+        
+
+        public static void KnowMe(string name, string color)
+        {
+            Console.WriteLine($"Hello, {name}. Your favourit color is {color}.");
+        }
+
+    }
+}
